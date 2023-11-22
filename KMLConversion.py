4f984feb06d15afaf0bc1bc91fee368247c9b3ab
Thomas Flaglor <tflaglor1@gmail.com>
2023-11-22 00:10:35 -0500

    reverted KMLConversion

--- KMLConversion.py
+++ KMLConversion.py
@@ -1,7 +1,5 @@
 import simplekml
 import pandas as pd
-from dataReader import dataCleaner, read_in_data_pynmea
-from clusteringTime import findHills
 
 kmlTop = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2"\n xmlns:gx="http://www.google.com/kml/ext/2.2">   <!-- required when using gx-prefixed elements -->\n\n<Placemark>\n  <name>gx:altitudeMode Example</name>\n  <LookAt>\n    <longitude>146.806</longitude>\n    <latitude>12.219</latitude>\n    <heading>-60</heading>\n    <tilt>70</tilt>\n    <range>6300</range>\n    <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>\n  </LookAt>\n  <LineString>\n    <extrude>1</extrude>\n    <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>\n    <coordinates>'
 kmlBot = '\n    </coordinates>\n  </LineString>\n</Placemark>\n</kml>'
@@ -17,17 +15,17 @@ def parseCoord(data):
 	return degrees + minutes / 60
 
 def getCoords(df):
-	lat = 1 if df["lat_dir"] == "N" else -1
-	long = 1 if df["lon_dir"] == "E" else -1
-	lat *= parseCoord(df["lat"])
-	long *= parseCoord(df["lon"])
+	lat = 1 if df["latitude_dir"] == "N" else -1
+	long = 1 if df["longitude_dir"] == "E" else -1
+	lat *= parseCoord(df["latitude"])
+	long *= parseCoord(df["longitude"])
 	return lat,long
 
 
 def extractAllCoords(df, indices):
 	coords = []
-	for row in indices:
-		lat, long = getCoords(row)
+	for i in indices:
+		lat, long = getCoords(df.loc[i])
 		coords.append((long, lat))
 	return coords
 
@@ -43,33 +41,29 @@ def convertToKML(df, hills,fileName):
 
 
 	for hill in hills:
-		segment_coords = extractAllCoords(df, hill)
+		segment_coords = extractAllCoords(df, range(hill[0], hill[1] + 1))
 		ls = kml.newlinestring(coords=segment_coords)
 		ls.style = uphillStyle
 
 	last = 0
 	for hill in hills:
-		start = hill[0]['altitude']
-		end = hill[len(hill)-1]['altitude']
+		start, end = hill
 		if start > last:
-			segment_coords = extractAllCoords(df, hill)
+			segment_coords = extractAllCoords(df, range(last, start))
 			ls = kml.newlinestring(coords=segment_coords)
 			ls.style = downhillStyle
 		last = end + 1
 
-	# if last < len(df):
-	# 	segment_coords = extractAllCoords(df, hills)
-	# 	ls = kml.newlinestring(coords=segment_coords)
-	# 	ls.style = downhillStyle
-	
-	
+	if last < len(df):
+		segment_coords = extractAllCoords(df, range(last, len(df)))
+		ls = kml.newlinestring(coords=segment_coords)
+		ls.style = downhillStyle
+
 	kml.save("outputs/"+fileName.strip(".txt")+".kml")
 
 
 
-if __name__ == "__main__":
-    filename = 'data/2023_08_14__210622_gps_file.txt'
-    df = read_in_data_pynmea(filename)
-    df = dataCleaner(df)
-    up = findHills(df)
-    convertToKML(df, up, filename)
+#if __name__ == "__main__":
+#     df = read_in_data_pynmea('data/2023_08_01__233842_gps_file.txt')
+#     df = dataCleaner(df)
+#     convertFileKML(df)
