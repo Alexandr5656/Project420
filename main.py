2c9c41e814b118acaf43c3632317c89053e3d17d
arb8590 <arb8590@rit.edu>
2023-11-22 02:07:33 -0500

    switched main to go through files

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from KMLConversion import convertFileKML
+from KMLConversion import convertToKML
 from dataReader import read_in_data
-from clusteringTime import labelHills
+from clusteringTime import findHills
 import time
+import os 
+def read_filenames_in_folder(folder_path):
+    try:
+        files_and_dirs = os.listdir(folder_path)
+        files = [f for f in files_and_dirs if os.path.isfile(os.path.join(folder_path, f))]
+        return files
+    except FileNotFoundError:
+        print(f"Folder not found: {folder_path}")
+        return []
 
+
+def createKML(fileName):
+
+	data = read_in_data("data/"+fileName)
+	up = findHills(data)#[:50000])
+	convertToKML(data,up,fileName)
 
 def main():
-	timeStart = time.perf_counter()
-	data = read_in_data('data/2023_08_01__233842_gps_file.txt')
-	df = labelHills(data)
-	convertFileKML(df)
-	endTime = time.perf_counter()
-	elapsed_time = endTime - timeStart
-	print(f"Time:{elapsed_time}")
+	folder_path = 'data'
+	file_names = read_filenames_in_folder(folder_path)
+	for file_name in file_names:
+	    createKML(file_name)
 
 if __name__ == "__main__":
 	main()
